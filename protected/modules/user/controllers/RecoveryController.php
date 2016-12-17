4eb7b1f172f8082324cb8b78ba0c4d82a89962ad
Akhmelik <Akhmelik>
2016-12-17 15:03:28 +0200

    mailsend fix

--- protected/modules/user/controllers/RecoveryController.php
+++ protected/modules/user/controllers/RecoveryController.php
@@ -53,22 +53,22 @@ class RecoveryController extends Controller
 			    						'{activation_url}'=>$activation_url,
 			    					));
 
-                            try {
-                                $mail = new YiiMailer();
-//$mail->clearLayout();//if layout is already set in config
-                                $mail->setFrom('support@eqbeat.ru', 'Support');
-                                $mail->setTo($user->email);
-                                $mail->setSubject($subject);
-                                $mail->setBody($message);
-                                $mail->send();
-                            } catch (phpmailerException $e) {
-                                echo $e->errorMessage(); //Pretty error messages from PHPMailer
-                            } catch (Exception $e) {
-                                echo $e->getMessage(); //Boring error messages from anything else!
-                            }
+//                            try {
+//                                $mail = new YiiMailer();
+////$mail->clearLayout();//if layout is already set in config
+//                                $mail->setFrom('support@eqbeat.ru', 'Support');
+//                                $mail->setTo($user->email);
+//                                $mail->setSubject($subject);
+//                                $mail->setBody($message);
+//                                $mail->send();
+//                            } catch (phpmailerException $e) {
+//                                echo $e->errorMessage(); //Pretty error messages from PHPMailer
+//                            } catch (Exception $e) {
+//                                echo $e->getMessage(); //Boring error messages from anything else!
+//                            }
 
 
-//			    			UserModule::sendMail($user->email,$subject,$message);
+			    			UserModule::sendMail($user->email,$subject,$message);
 			    			
 							Yii::app()->user->setFlash('recoveryMessage',UserModule::t("Please check your email. An instructions was sent to your email address."));
 			    			$this->refresh();
