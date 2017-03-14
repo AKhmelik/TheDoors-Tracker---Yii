0ae694ae30bd15d4235406499addeee0ff4ffda7
Akhmelik <Akhmelik>
2017-03-14 21:59:44 +0200

    utc test

--- protected/views/metric/index.php
+++ protected/views/metric/index.php
@@ -204,8 +204,8 @@ echo CHtml::scriptFile(Yii::app()->request->baseUrl . "/js/click-handler.js");
                     }
                 },
                 function(start, end, label) {
-                    $('input[name=startData]').val(start.format('YYYY-MM-DD h:mm:ss'));
-                    $('input[name=endDate]').val(end.format('YYYY-MM-DD h:mm:ss'));
+                    $('input[name=startData]').val(moment(start).utc.format('YYYY-MM-DD h:mm:ss'));
+                    $('input[name=endDate]').val(moment(end).utc.format('YYYY-MM-DD h:mm:ss'));
                 });
             $('#search-query-main').popover({ trigger: "hover" });
             $('#user-select').popover({ trigger: "hover" });
